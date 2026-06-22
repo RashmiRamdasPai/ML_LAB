9ac66ee67c43f8b8036f33ba0ef7356813a38ed6
Rashmi Ramdas Pai <161941092+RashmiRamdasPai@users.noreply.github.com>
2026-06-22 19:42:05 +0530

    Update multilayer_perceptron.py

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -4,24 +4,16 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def mlp(x, w1, b1, w2, b2):
-
-    # Hidden Layer
     hidden = sigmoid(np.dot(x, w1) + b1)
-
-    # Output Layer
     output = sigmoid(np.dot(hidden, w2) + b2)
-
     return 1 if output >= 0.5 else 0
 
 X = np.array([[0,0],[0,1],[1,0],[1,1]])
 
 # AND Gate
 print("AND Gate")
-
-w1 = np.array([[1,1],
-               [1,1]])
-b1 = np.array([-0.5,-0.5])
-
+w1 = np.array([[1,1],[1,1]])
+b1 = np.array([-1.5,-1.5])
 w2 = np.array([1,1])
 b2 = -1.5
 
@@ -30,7 +22,7 @@ for x in X:
 
 # OR Gate
 print("\nOR Gate")
-
+b1 = np.array([-0.5,-0.5])
 b2 = -0.5
 
 for x in X:
